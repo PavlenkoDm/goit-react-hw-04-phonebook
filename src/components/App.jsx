57faaa0cc37dd364449cbc90f51d5d24475d61a7
PavlenkoDm <dmpavlenko07@gmail.com>
2023-05-01 13:43:22 +0300

    udate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#2c2b2b'
       }}
     >
       <Phonebook/>
